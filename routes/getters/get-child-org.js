c7946a5895345040cb3c6d73478d89acee998fac
JustinMonty20 <montgomerycodepro@gmail.com>
2021-10-05 15:29:27 -0400

    added comment about the behavior of a findChildOrg controller function

--- routes/getters/get-child-org.js
+++ routes/getters/get-child-org.js
@@ -4,8 +4,14 @@ const mongoFactory  = require('../../helper/db-factory.js');
 
 const mongoDB = mongoFactory(process.env.MONGO_URI);
 
+// not sure I'll need this as a route maybe just findChildOrg as a helper to add a warehouse and push to that organization.
 router.get('/:childName', async (req, res) => {
     try {
+        /**
+         * doesn't work how I thought it would.
+         * I thought it would grab just the child object and return it
+         * grabs the whole document though no big deal.
+         */
         mongoDB.connect();
         console.log(req.params);
         const childCompany = await findChildOrg(req.params);
